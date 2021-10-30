d50af49f760060d6c5ce51a7da93fc4943ad1463
HanJiayi <jiayi.han@thoughtworks.com>
2021-10-31 02:37:16 +0800

    [hanjiayi] js homework part two

---
+++ src/todolist.js
@@ -0,0 +1,125 @@
+let allItems = []
+
+function addTodoItem(e) {
+  let inputText = document.getElementById('item-input-text').value
+  if (e.keyCode !== 13) {
+    return
+  }
+  clearRadioStatus()
+  let li = document.createElement('li')
+  li.setAttribute(
+    'style',
+    'font-size: 2rem; padding-top: 0.6rem; word-break: break-all;'
+  )
+
+  let checkbox = document.createElement('input')
+  checkbox.setAttribute('type', 'checkbox')
+  checkbox.setAttribute('name', 'todo-item')
+  checkbox.setAttribute('onclick', 'checkboxOnclick(this)')
+  checkbox.setAttribute('style', 'margin: 3px; transform: scale(1/1.4, 1/1.4);')
+  li.appendChild(checkbox)
+  let label = document.createElement('label')
+  label.setAttribute('style', 'margin-left:0.5rem;')
+  label.innerText = inputText.trim()
+  li.appendChild(label)
+  allItems.push(li)
+  if (!document.getElementById('radio-finished').checked) {
+    document.getElementById('todo-item-ul-id').appendChild(li)
+  }
+  document.getElementById('item-input-text').value = ''
+  getLiCount()
+}
+
+function checkboxOnclick(checkbox) {
+  checkbox.nextElementSibling.setAttribute('style', 'color: #000000;')
+  if (!document.getElementById('radio-all').checked) {
+    document
+      .getElementById('todo-item-ul-id')
+      .removeChild(checkbox.parentElement)
+  }
+  if (checkbox.checked === true) {
+    checkbox.nextElementSibling.setAttribute(
+      'style',
+      'color: #808080; text-decoration:line-through;'
+    )
+  }
+  getLiCount()
+}
+
+function clearRadioStatus() {
+  const siblings = Array.from(document.getElementById('radio-id').children)
+  siblings.forEach((each) => {
+    if (each.className === 'radio-item') {
+      return each.nextElementSibling.setAttribute('style', 'border:0')
+    }
+  })
+}
+function radioChange(radio) {
+  clearRadioStatus()
+  radio.setAttribute('checked', true)
+  radio.nextElementSibling.setAttribute('style', 'border:1px solid darkorange;')
+  let liItems = document.getElementById('todo-item-ul-id')
+  liItems.innerHTML = ''
+  allItems.forEach((item) => {
+    liItems.appendChild(item)
+    if (item.firstChild.checked && radio.value === 'Active') {
+      liItems.removeChild(item)
+    }
+    if (!item.firstChild.checked && radio.value === 'Finished') {
+      liItems.removeChild(item)
+    }
+    if (item.firstChild.checked && radio.value === 'Clear completed') {
+      liItems.removeChild(item)
+      console.log('item', item.innerText)
+      console.log('allItems', allItems)
+      allItems = allItems.filter((each) => {
+        return each.outerText !== item.innerText
+      })
+    }
+  })
+  getLiCount()
+}
+
+function getLiCount() {
+  const countUI = document.getElementById('total-count')
+  countUI.innerText = ''
+  const ul = document.getElementById('todo-item-ul-id')
+  const countData = ul.childNodes.length
+  const spanUI = document.createElement('span')
+  spanUI.setAttribute(
+    'style',
+    'height: 1rem; font-size: 1rem; display:inline-block;line-height: 3rem;margin-left: 0.5rem;'
+  )
+  spanUI.innerText =
+    countData + (countData > 1 ? '   items left' : '   item left')
+  countUI.appendChild(spanUI)
+}
+
+function setVisible() {
+  const trian = document.getElementById('content')
+  const value = trian.getAttribute('value');
+  const list = document.getElementsByClassName('todo-item-list')[0];
+  const tool = document.getElementsByClassName('toolbar')[0];
+  console.log('start',value)
+  if (value.length === 0) {
+    trian.setAttribute(
+      'style',
+      'width: 10px;height: 10px; border:#666 solid; border-width: 3px 3px 0 0; transform: rotate(-45deg);margin-bottom: 10px;'
+    )
+    trian.setAttribute('value','1');
+    list.setAttribute('style','visibility:hidden;position:absolute;')
+    tool.setAttribute('style','visibility:hidden;position:absolute;')
+
+  }else{
+    trian.setAttribute(
+        'style',
+        'width: 10px;height: 10px; border:#666 solid; border-width: 3px 3px 0 0; transform: rotate(135deg);margin-bottom: 10px;'
+      )
+      trian.setAttribute('value','');
+      list.setAttribute('style','visibility:visible')
+      tool.setAttribute('style','visibility:visible')
+
+  }
+  
+  console.log('end',value)
+}
